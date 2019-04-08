396ee14e0f2bfc0a41afa67c370e53fb09227a71
Cameron Podd <podd@usc.edu>
2019-04-08 01:50:41 -0700

    Updates

---
+++ precache-manifest.b99837cc7a1558b9b5884e1e75d799b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "768365787f3304def57a",
+    "url": "/OrganizedNotesFE/static/css/main.1bbe48d2.chunk.css"
+  },
+  {
+    "revision": "768365787f3304def57a",
+    "url": "/OrganizedNotesFE/static/js/main.4599dcf7.chunk.js"
+  },
+  {
+    "revision": "508da87080c957eb7374",
+    "url": "/OrganizedNotesFE/static/js/runtime~main.3fa23d9a.js"
+  },
+  {
+    "revision": "f3aeceb620ced686009a",
+    "url": "/OrganizedNotesFE/static/js/2.b66200a5.chunk.js"
+  },
+  {
+    "revision": "d86f866089c790d9fecce5e6e0c6431c",
+    "url": "/OrganizedNotesFE/static/media/arrow.d86f8660.png"
+  },
+  {
+    "revision": "21691ecf551fc1c22553f0ccfa141bd7",
+    "url": "/OrganizedNotesFE/static/media/trash.21691ecf.png"
+  },
+  {
+    "revision": "4c3c5df92631b86b45c056663b0869a0",
+    "url": "/OrganizedNotesFE/static/media/edit.4c3c5df9.png"
+  },
+  {
+    "revision": "b6acdd9165439644419db8edec2531b4",
+    "url": "/OrganizedNotesFE/static/media/Logo.b6acdd91.jpg"
+  },
+  {
+    "revision": "a4b5a2c3a814ed3338b2b0fcf62a8af5",
+    "url": "/OrganizedNotesFE/index.html"
+  }
+];
